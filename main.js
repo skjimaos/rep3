fb99fe8fbb402b5ab6d0d206f668f91969f1c311
la-lavadora <62385493+la-lavadora@users.noreply.github.com>
2020-05-08 14:32:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -116,7 +116,6 @@ $(function() {
     for (var i = 0; i < window.settings.numberofavatars; i++) {
       foo.push(i);
     } 
-    var sfoo = shuffle(foo);
   	for(var i in sfoo) 
   	{ 
   		$('.avatars').append('<img id="avatar_' + foo[i] + '" src="avatars/avatar_' + i + '.png" class="avatar" />');
