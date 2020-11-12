9e4645105c740da13a889d8fa3ce920f86a8754e
LauraKathariana <marbacher.laura@hotmail.ch>
2020-11-12 12:01:55 +0100

    Übersetzung 5

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ $(function() {
 	
     // Usernames by which the participant will receive "likes"
 	// If group member names are changed, these should be changed accordingly.
-    settings.likes_by = ['John','AncaD','Sarah','Arjen','Jane','George','Dan','Heather','Ky']; 
+    settings.likes_by = ['Johannes','AncaD','Sarah','Rea','Janette','Gregor','Dan','Mirelle','Ky']; 
   }
   
   // -------------------
