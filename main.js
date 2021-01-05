cc60e961ebefa1aae381f8eb9f7fea17a709e910
la-lavadora <62385493+la-lavadora@users.noreply.github.com>
2021-01-05 10:32:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ $(function() {
     settings.condition_1_likes = [12000, 18000]; 
 
     // In condition 2, user will receive 6 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_2_likes = [12000, 18000, 25000, 35000, 50000];  
+    settings.condition_2_likes = [12000, 18000, 25000, 35000, 50000, 68000];  
     
     // In condition 3, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
     settings.condition_3_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000]; 
