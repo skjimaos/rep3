3109bd24a90644a7c085eeeeefd8fab318e7d846
la-lavadora <62385493+la-lavadora@users.noreply.github.com>
2021-01-05 10:09:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -36,10 +36,10 @@ $(function() {
 	// In cases with only 1 "like," a second "like" is added with time point 9999999. This "like" is added for programming purposes and is never executed, as it is outside the task time
 
     // In condition 1, the participant will receive 1 like at the following timepoint (in ms). Default: [12000, 9999999]
-    settings.condition_1_likes = [12000, 30000]; 
+    settings.condition_1_likes = [12000, 9999999]; 
 
     // In condition 2, user will receive 6 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
-    settings.condition_2_likes = [12000, 30000, 40000, 45000, 60000, 80000 ];  
+    settings.condition_2_likes = [10000, 15000,35000,80000,1320000,150000];  
     
     // In condition 3, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
     settings.condition_3_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000]; 
@@ -48,7 +48,7 @@ $(function() {
 	// To keep the total distribution of "likes" constant across conditions, The "likes" received by one group member can be adjusted according to the participant's. By default, the other group member receives 9 "likes" in the participant-ostracism condition, 5 in the participant-inclusion condtion, and 1 in the participant-overinclusion condtion.
 	 settings.condition_1_adjusted_likes = [12000, 14000,15000,35000,80000,100000,110000,150000,20000]; // 9
 	 settings.condition_2_adjusted_likes = [12000, 14000,15000,35000,80000]; // 5
-	 settings.condition_3_adjusted_likes = [12000, 9999999]; //1	
+	 settings.condition_3_adjusted_likes = [12000, 18000]; //1	
 	
     // Usernames by which the participant will receive "likes"
 	// If group member names are changed, these should be changed accordingly.
