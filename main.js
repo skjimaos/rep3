30f6a6423ec39c5a1165c0b4520235bd6635ba7d
LauraKathariana <marbacher.laura@hotmail.ch>
2022-01-03 18:04:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -44,11 +44,20 @@ $(function() {
     // In condition 3, user will receive 9 likes at the following timepoints (in ms). Default: [10000, 11000,15000,35000,80000,100000,110000,150000,20000]
     settings.condition_3_likes = [10000, 11000,15000,35000,80000,100000,110000,150000,20000]; 
 
+    // In condition 4, the participant will receive 1 like at the following timepoint (in ms). Default: [12000, 9999999]
+  	settings.condition_4_likes = [12000, 9999999]; 
+
+    // In condition 5, user will receive 6 likes at the following timepoints (in ms). Default: [10000, 15000,35000,80000,1320000,150000]
+    settings.condition_5_likes = [18000, 25000, 29000, 35000, 50000, 68000, 72000 ];  
+    
 	// **Others' likes**     
 	// To keep the total distribution of "likes" constant across conditions, The "likes" received by one group member can be adjusted according to the participant's. By default, the other group member receives 9 "likes" in the participant-ostracism condition, 5 in the participant-inclusion condtion, and 1 in the participant-overinclusion condtion.
 	 settings.condition_1_adjusted_likes = [12000, 14000,15000,35000,80000,100000,110000,150000,20000]; // 9
 	 settings.condition_2_adjusted_likes = [12000, 14000,15000,35000,80000]; // 5
-	 settings.condition_3_adjusted_likes = [12000, 9999999]; //1	
+	 settings.condition_3_adjusted_likes = [12000, 9999999]; //1
+	 settings.condition_4_adjusted_likes = [12000, 14000,15000,35000,80000,100000,110000,150000,20000]; // 9
+	 settings.condition_5_adjusted_likes = [12000, 14000,15000,35000,80000]; // 5
+
 	
     // Usernames by which the participant will receive "likes"
 	// If group member names are changed, these should be changed accordingly.
@@ -382,12 +391,12 @@ $(function() {
 			window.others.posts[1].likes = settings.condition_3_adjusted_likes;
 			break;
 		case 4:
-			window.settings.condition_likes = settings.condition_2_likes;
-			window.others.posts[1].likes = settings.condition_2_adjusted_likes;
+			window.settings.condition_likes = settings.condition_4_likes;
+			window.others.posts[1].likes = settings.condition_5_adjusted_likes;
 			break;
 		case 5:
-			window.settings.condition_likes = settings.condition_3_likes;
-			window.others.posts[1].likes = settings.condition_3_adjusted_likes;
+			window.settings.condition_likes = settings.condition_5_likes;
+			window.others.posts[1].likes = settings.condition_5_adjusted_likes;
 			break;
 	}	
 	  
