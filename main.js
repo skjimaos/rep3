077c64fc3e9ea60b63d4a495873514db086f4c7f
la-lavadora <62385493+la-lavadora@users.noreply.github.com>
2022-02-18 17:58:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ $(function() {
 	
     // Usernames by which the participant will receive "likes"
 	// If group member names are changed, these should be changed accordingly.
-    settings.likes_by = ['Julia','Felix','Sarah','Max','Nicola']; 
+    settings.likes_by = ['Julia','Felix','Sarah','Jessica','Nicola']; 
   }
   
   // -------------------
